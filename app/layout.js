000a50b512fbf512e6188815788aa5cae05b1748
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-29 10:20:42 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import "@/styles/global.css"
+
+export const metadata = {
+    title: "Sudarshan Mhasrup",
+    description: "A software developer who likes to build mobile applications.",
+};
+
+export default function RootLayout({children}) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
+}
